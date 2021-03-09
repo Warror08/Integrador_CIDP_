e576def0227ef14bec6eb1bc66fd87e396a24929
Arielesales <ariele.teixeira@escolar.ifrn.edu.br>
2021-03-09 20:42:42 -0300

    saude

--- application/views/novo_gasto.php
+++ application/views/novo_gasto.php
@@ -1,9 +1,9 @@
 <!-- fazer commit -->
-    <h1>Novo Gasto</h1>
+    <h1>Adicionar nova denuncia de saúde</h1>
     <?= form_open('gastos/salvar') ?>
         <label for="data">Data: <input type="date" name="data"></label><br/>
+        <label for="assunto">Assunto: <input type="text" name="assunto"></label><br/>
         <label for="descricao">Descrição: <input type="text" name="descricao"></label><br/>
-        <label for="valor">Valor: <input type="number" name="valor"></label><br/>
         <button type="submit">Enviar</button>
         <button type="reset">Limpar</button>
     <?= form_close() ?>
