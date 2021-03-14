b9dcb6dfa108eee6db18af86ad4702ebbc3ee1fa
Arielesales <ariele.teixeira@escolar.ifrn.edu.br>
2021-03-14 13:11:00 -0300

    menu lateraç

--- application/views/cabecalho.php
+++ application/views/cabecalho.php
@@ -1,86 +1,70 @@
-<!doctype html>
-<html lang="en" class="h-100">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.80.0">
-    <title><?= $titulo ?></title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+ *{
+   margin: 0px;
+ }
+a{
+    text-decoration: none;
+    color: black;
+}
+    
+li:hover {
+  background-color: yellow;
+  color:blue;
+}
+ul{
+  background-color: ;
+  top:70px;
+  position: absolute;
+  width:;
+}
+ul{
+    list-style: none;
+}
+body{
+    font-family: arial;
+}
+img{
+    width: 40px;
+}
 
-
-
-    <!-- Bootstrap core CSS -->
-<link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-<link rel="icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/favicon.ico">
-
-<link href="<?= base_url('assets/css/estilo.css')?>" rel="stylesheet">
-
-<meta name="theme-color" content="#7952b3">
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-/**Fazer commit */
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
+input{
+      display: none;
+}
+nav{
+      background-color: rgba(16,16,16,.5);
+      width: 350px;
+      height: 100%;
+}
+   label{
+     background-color: red;
+     padding: ;
+     position: absolute;
+   }
     </style>
 
-    
-    <!-- Custom styles for this template -->
-    <link href="sticky-footer-navbar.css" rel="stylesheet">
-  </head>
-  <body class="d-flex flex-column h-100">
-    
-<header class="sticky-top">
-  <!-- Fixed navbar -->
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Fixed navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?= base_url('gastos/index')?>">Iluminação</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('gastos/novo')?>">Saúde</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('categorias/index')?>" tabindex="-1" aria-disabled="true">Educação</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('usuario/logoff')?>" tabindex="-1" aria-disabled="true">Sair</a>
-          </li>
-        </ul>
-        <div class="d-flex">
-          <p> <?= $this->session->userdata('usuario') ?> </p>
-        </div>
-      </div>
-    </div>
+</head>
+<body>
+<input type="checkbox" id="chec"> 
+<label for="chec">
+<img src="https://clipartart.com/images/hamburger-menu-icon-clipart-3.png" alt="">
+</label>
+</input>
+  <nav>
+  <ul>
+ <li><a href="">INICIO</a></li>
+ <li><a href="">SAÚDE</a></li>
+ <li><a href="">EDUCAÇÃO</a></li>
+ <li><a href="">ILUMINAÇÃO</a></li>
+ <li><a href="">SUAS DENÚNCIAS</a></li>
+  </ul>
   </nav>
-</header>
-
-<!-- Begin page content -->
-<main class="">
-  <div class="container">
+</body>
+</html>
